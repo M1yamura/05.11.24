592e546257f5062e29da6d073abcf79eaa59383b
Andrey <157319383+M1yamura@users.noreply.github.com>
2024-11-06 11:49:00 +0300

    Create .cpp

---
+++ 3/.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double v, t, a, S;
+    cout << "Введите скорость (v), время (t) и ускорение (a): ";
+    cin >> v >> t >> a;
+
+    S = v * t + (a * t * t) / 2;
+    cout << "Пройденное расстояние S: " << S << " метров" << endl;
+
+    return 0;
+}
