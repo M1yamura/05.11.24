dff937e8892627c065b15773439d37ee12104275
Andrey <157319383+M1yamura@users.noreply.github.com>
2024-11-06 12:22:42 +0300

    Update 3.cpp

--- 3.cpp
+++ 3.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main() {
     double v, t, a, S;
-    cout << "Введите скорость (v), время (t) и ускорение (a): ";
+    cout << "Введите скорость(v), время(t) и ускорение(a): ";
     cin >> v >> t >> a;
 
     S = v * t + (a * t * t) / 2;
