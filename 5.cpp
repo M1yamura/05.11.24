8276246e9ed0f8bd8886325bfffc2ce220bda963
Andrey <157319383+M1yamura@users.noreply.github.com>
2024-11-06 12:20:54 +0300

    Update 5.cpp

--- 5.cpp
+++ 5.cpp
@@ -4,11 +4,11 @@ using namespace std;
 
 int main() {
     double amount;
-    cout << "Введите сумму: ";
+    cout << "Число";
     cin >> amount;
 
     int dollars = static_cast<int>(amount);
-    int cents = static_cast<int>((amount - dollars) * 100 + 0.5); // округление
+    int cents = static_cast<int>((amount - dollars) * 100 + 0.5); 
 
     cout << dollars << " долларов и " << cents << " центов" << endl;
 
