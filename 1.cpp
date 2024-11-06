4bd56abce9a659a2b5c08124abbede5436107a53
Andrey <157319383+M1yamura@users.noreply.github.com>
2024-11-06 14:11:43 +0300

    Update 1.cpp

--- 1.cpp
+++ 1.cpp
@@ -2,9 +2,13 @@
 using namespace std;
 
 int main() {
+    setlocale(LC_ALL,"ru");
     double R1, R2, R3, R0;
-    cout << "Введите сопротивления R1, R2, R3: ";
-    cin >> R1 >> R2 >> R3;
+    std::cout << "Введите  R1, R2, R3: \n ";
+    std::cin >> R1 >> R2 >> R3;
+    if (R1 == 0 || R2 == 0 || R3 == 0) {
+        std::cout << "Error: d.0\n";
+            return 1;}
     R0 = 1 / (1 / R1 + 1 / R2 + 1 / R3);
     cout << "Эквивалентное сопротивление R0: " << R0 << " Ом" << endl;
 
