e33ca7f21780f5b680330538f6434057a88f43c4
Andrey <157319383+M1yamura@users.noreply.github.com>
2024-11-06 12:14:09 +0300

    Update 7.cpp

--- 7.cpp
+++ 7.cpp
@@ -3,28 +3,28 @@
 using namespace std;
 
 int main() {
-    double distance, time_minutes, time_seconds, total_time_seconds, speed;
+    double distance, time_input, time_seconds, speed;
 
     cout << "Calculating running speed." << endl;
     cout << "Enter the length of distance (meters) = ";
     cin >> distance;
 
-    cout << "Enter time (min.sec) = ";
-    cin >> time_minutes >> time_seconds;
+    cout << "Enter time= ";
+    cin >> time_input;
 
-    // Преобразуем время в секунды
-    total_time_seconds = time_minutes * 60 + time_seconds;
+    int minutes = static_cast<int>(time_input); 
+    double fractional_part = time_input - minutes;
+    int seconds = static_cast<int>(fractional_part * 60); 
 
-    // Вычисляем скорость в м/с
-    speed = distance / total_time_seconds;
+    time_seconds = minutes * 60 + seconds;
 
-    // Преобразуем скорость в км/ч
+    speed = distance / time_seconds;
+
     speed *= 3.6;
 
     cout << "Distance: " << distance << " m" << endl;
-    cout << "Time: " << static_cast<int>(time_minutes) << " min " 
-         << static_cast<int>(time_seconds) << " sec = " 
-         << total_time_seconds << " seconds" << endl;
+    cout << "Time: " << minutes << " min " << seconds << " sec = " 
+         << time_seconds << " seconds" << endl;
     cout << "You were running at speed " << fixed << setprecision(2) 
          << speed << " km/h" << endl;
 
