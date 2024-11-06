534f0c8d1b2404e93ed559630328a6bbb79b677a
Andrey <157319383+M1yamura@users.noreply.github.com>
2024-11-06 12:23:55 +0300

    Update 2.cpp

--- 2.cpp
+++ 2.cpp
@@ -1,14 +1,12 @@
 #include <iostream>
 #include <cmath>
 using namespace std;
-
 const double PI = 3.14159265358979323846;
 
 int main() {
     double L, R, S;
-    cout << "Введите длину окружности: ";
+    cout << "Длина окружности: ";
     cin >> L;
-
     R = L / (2 * PI);
     S = PI * R * R;
     cout << "Площадь круга: " << S << endl;
